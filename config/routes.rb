35dc681adb7c4faac63b130286f0e42fd162302e
Benedict O'Mahoney <benedict@benedict.com>
2019-11-03 13:18:25 -0800

    add devise and active_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get 'home/index'
   root :to => 'home#index'
 end
